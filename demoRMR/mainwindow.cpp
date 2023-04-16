08eaf0504d0ac7f7dc067edadc13a76e0993ec8e
pao3007 <lukap1999@gmail.com>
2023-04-16 15:30:19 +0200

    okno

---
+++ demoRMR/mainwindow.cpp
@@ -0,0 +1,258 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QPainter>
+#include <math.h>
+///TOTO JE DEMO PROGRAM...AK SI HO NASIEL NA PC V LABAKU NEPREPISUJ NIC,ALE SKOPIRUJ SI MA NIEKAM DO INEHO FOLDERA
+/// AK HO MAS Z GITU A ROBIS NA LABAKOVOM PC, TAK SI HO VLOZ DO FOLDERA KTORY JE JASNE ODLISITELNY OD TVOJICH KOLEGOV
+/// NASLEDNE V POLOZKE Projects SKONTROLUJ CI JE VYPNUTY shadow build...
+/// POTOM MIESTO TYCHTO PAR RIADKOV NAPIS SVOJE MENO ALEBO NEJAKY INY LUKRATIVNY IDENTIFIKATOR
+/// KED SA NAJBLIZSIE PUSTIS DO PRACE, SKONTROLUJ CI JE MIESTO TOHTO TEXTU TVOJ IDENTIFIKATOR
+/// AZ POTOM ZACNI ROBIT... AK TO NESPRAVIS, POJDU BODY DOLE... A NIE JEDEN,ALEBO DVA ALE BUDES RAD
+/// AK SA DOSTANES NA SKUSKU
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+
+    //tu je napevno nastavena ip. treba zmenit na to co ste si zadali do text boxu alebo nejaku inu pevnu. co bude spravna
+    ipaddress="127.0.0.1";//192.168.1.11toto je na niektory realny robot.. na lokal budete davat "127.0.0.1"
+  //  cap.open("http://192.168.1.11:8000/stream.mjpg");
+    ui->setupUi(this);
+    datacounter=0;
+  //  timer = new QTimer(this);
+//    connect(timer, SIGNAL(timeout()), this, SLOT(getNewFrame()));
+    actIndex=-1;
+    useCamera1=false;
+
+
+
+
+    datacounter=0;
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    QSize size = this->size();
+    static int previousHeight = 0;
+    static int previousWidth = 0;
+    ///prekreslujem obrazovku len vtedy, ked viem ze mam nove data. paintevent sa
+    /// moze pochopitelne zavolat aj z inych dovodov, napriklad zmena velkosti okna
+    painter.setBrush(Qt::black);//cierna farba pozadia(pouziva sa ako fill pre napriklad funkciu drawRect)
+    QPen pero;
+    pero.setStyle(Qt::SolidLine);//styl pera - plna ciara
+    pero.setWidth(3);//hrubka pera -3pixely
+    pero.setColor(Qt::green);//farba je zelena
+    QRect rect;
+
+    rect= ui->frame->geometry();//ziskate porametre stvorca,do ktoreho chcete kreslit
+    rect.translate(0,15);
+    painter.drawRect(rect);
+
+    QRect rectMini(15,ui->frame->pos().y()+20,ui->frame->geometry().width()/4,ui->frame->geometry().height()/4);
+    ui->pushButton_image->setGeometry(3,7,ui->frame->geometry().width()/4,ui->frame->geometry().height()/4);
+    ui->pushButton_image->setStyleSheet("background-color: rgba(255, 255, 255, 0);");
+
+
+
+    static QColor rectColor(255, 0, 0, 75);
+    painter.fillRect(rectMini,rectColor);
+
+
+    float pomer = (float)size.width() / (float)size.height();
+
+    if((previousHeight-size.height()) < 0 || (previousHeight-size.width()) < 0){
+        if(!((1.75 > pomer) && (pomer > 1.79))){
+            if(pomer < 1.75){
+                int width = size.height()*1.77;
+                resize(width,size.height());
+            }
+            if(pomer > 1.79){
+                int height = size.width()/1.77;
+                resize(size.width(),height);
+            }
+        }
+         ui->pushButton_9->setGeometry((size.width()/2)-100,size.height()-90,201,31);
+    }else if((previousHeight-size.height()) > 0 || (previousHeight-size.width()) > 0){
+        if(!((1.75 > pomer) && (pomer > 1.79))){
+            if(pomer < 1.75){
+                int height = size.width()/1.77;
+                resize(size.width(),height);
+            }
+            if(pomer > 1.79){
+                int width = size.height()*1.77;
+                resize(width,size.height());
+            }
+        }
+         ui->pushButton_9->setGeometry((size.width()/2)-100,size.height()-90,201,31);
+    }
+
+    previousHeight = size.height();
+    previousWidth = size.width();
+
+    if(actIndex>-1)/// ak zobrazujem data z kamery a aspon niektory frame vo vectore je naplneny
+    {
+        std::cout<<actIndex<<std::endl;
+        QImage image = QImage((uchar*)frame[actIndex].data, frame[actIndex].cols, frame[actIndex].rows, frame[actIndex].step, QImage::Format_RGB888  );//kopirovanie cvmat do qimage
+        if(pushBtnImg){
+            painter.drawImage(rectMini,image.rgbSwapped());
+        }else painter.drawImage(rect,image.rgbSwapped());
+
+    }
+
+
+}
+
+
+/// toto je slot. niekde v kode existuje signal, ktory je prepojeny. pouziva sa napriklad (v tomto pripade) ak chcete dostat data z jedneho vlakna (robot) do ineho (ui)
+/// prepojenie signal slot je vo funkcii  on_pushButton_9_clicked
+void  MainWindow::setUiValues(double robotX,double robotY,double robotFi)
+{
+     ui->lineEdit_2->setText(QString::number(robotX));
+     ui->lineEdit_3->setText(QString::number(robotY));
+     ui->lineEdit_4->setText(QString::number(robotFi));
+}
+
+///toto je calback na data z robota, ktory ste podhodili robotu vo funkcii on_pushButton_9_clicked
+/// vola sa vzdy ked dojdu nove data z robota. nemusite nic riesit, proste sa to stane
+int MainWindow::processThisRobot(TKobukiData robotdata)
+{
+
+
+
+
+    return 0;
+
+}
+
+///toto je calback na data z lidaru, ktory ste podhodili robotu vo funkcii on_pushButton_9_clicked
+/// vola sa ked dojdu nove data z lidaru
+int MainWindow::processThisLidar(LaserMeasurement laserData)
+{
+
+
+    memcpy( &copyOfLaserData,&laserData,sizeof(LaserMeasurement));
+    //tu mozete robit s datami z lidaru.. napriklad najst prekazky, zapisat do mapy. naplanovat ako sa prekazke vyhnut.
+    // ale nic vypoctovo narocne - to iste vlakno ktore cita data z lidaru
+    updateLaserPicture=1;
+    update();//tento prikaz prinuti prekreslit obrazovku.. zavola sa paintEvent funkcia
+
+
+    return 0;
+
+}
+
+///toto je calback na data z kamery, ktory ste podhodili robotu vo funkcii on_pushButton_9_clicked
+/// vola sa ked dojdu nove data z kamery
+int MainWindow::processThisCamera(cv::Mat cameraData)
+{
+
+    cameraData.copyTo(frame[(actIndex+1)%3]);//kopirujem do nasej strukury
+    actIndex=(actIndex+1)%3;//aktualizujem kde je nova fotka
+    updateLaserPicture=1;
+    return 0;
+}
+void MainWindow::on_pushButton_9_clicked() //start button
+{
+
+    forwardspeed=0;
+    rotationspeed=0;
+    //tu sa nastartuju vlakna ktore citaju data z lidaru a robota
+    connect(this,SIGNAL(uiValuesChanged(double,double,double)),this,SLOT(setUiValues(double,double,double)));
+
+    ///setovanie veci na komunikaciu s robotom/lidarom/kamerou.. su tam adresa porty a callback.. laser ma ze sa da dat callback aj ako lambda.
+    /// lambdy su super, setria miesto a ak su rozumnej dlzky,tak aj prehladnost... ak ste o nich nic nepoculi poradte sa s vasim doktorom alebo lekarnikom...
+    robot.setLaserParameters(ipaddress,52999,5299,/*[](LaserMeasurement dat)->int{std::cout<<"som z lambdy callback"<<std::endl;return 0;}*/std::bind(&MainWindow::processThisLidar,this,std::placeholders::_1));
+    robot.setRobotParameters(ipaddress,53000,5300,std::bind(&MainWindow::processThisRobot,this,std::placeholders::_1));
+    //---simulator ma port 8889, realny robot 8000
+    robot.setCameraParameters("http://"+ipaddress+":8889/stream.mjpg",std::bind(&MainWindow::processThisCamera,this,std::placeholders::_1));
+
+    ///ked je vsetko nasetovane tak to tento prikaz spusti (ak nieco nieje setnute,tak to normalne nenastavi.cize ak napr nechcete kameru,vklude vsetky info o nej vymazte)
+    robot.robotStart();
+
+
+
+    //ziskanie joystickov
+    instance = QJoysticks::getInstance();
+
+
+    /// prepojenie joysticku s jeho callbackom... zas cez lambdu. neviem ci som to niekde spominal,ale lambdy su super. okrem toho mam este rad ternarne operatory a spolocneske hry ale to tiez nikoho nezaujima
+    /// co vas vlastne zaujima? citanie komentov asi nie, inak by ste citali toto a ze tu je blbosti
+    connect(
+        instance, &QJoysticks::axisChanged,
+        [this]( const int js, const int axis, const qreal value) { if(/*js==0 &&*/ axis==1){forwardspeed=-value*300;}
+            if(/*js==0 &&*/ axis==0){rotationspeed=-value*(3.14159/2.0);}}
+    );
+}
+
+void MainWindow::on_pushButton_2_clicked() //forward
+{
+    //pohyb dopredu
+    robot.setTranslationSpeed(500);
+
+}
+
+void MainWindow::on_pushButton_3_clicked() //back
+{
+    robot.setTranslationSpeed(-250);
+
+}
+
+void MainWindow::on_pushButton_6_clicked() //left
+{
+robot.setRotationSpeed(3.14159/2);
+
+}
+
+void MainWindow::on_pushButton_5_clicked()//right
+{
+robot.setRotationSpeed(-3.14159/2);
+
+}
+
+void MainWindow::on_pushButton_4_clicked() //stop
+{
+    robot.setTranslationSpeed(0);
+
+}
+
+void MainWindow::on_pushButton_image_clicked() //stop
+{
+    if(pushBtnImg){
+        pushBtnImg = false;
+    }else pushBtnImg = true;
+
+}
+
+
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    if(useCamera1==true)
+    {
+        useCamera1=false;
+
+        ui->pushButton->setText("use camera");
+    }
+    else
+    {
+        useCamera1=true;
+
+        ui->pushButton->setText("use laser");
+    }
+}
+
+void MainWindow::getNewFrame()
+{
+
+}
